3d76c74265d1f50ca561eb2973e279af9315d1a9
sanjay.srinivas <f0r3v3ran00b@gmail.com>
2019-02-21 17:05:59 +1100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        defaultText: "default text",
+        inputValue: "",
+        labelText: ""
+    }
+  }
+
+  handleValueChange = (event) => {
+
+      let inputValue = event.target.value;
+      console.log(`Change... ${inputValue}`)
+      this.setState({
+          labelText: inputValue
+      });
+  }
+
+
   render() {
     return (
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <p />
+          <input placeholder={this.state.defaultText} type="text" onChange={this.handleValueChange} />
+          <div>{this.state.labelText}</div>
       </div>
     );
   }
